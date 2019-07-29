0ed1c68f8624bec200efe4eced9f96c330e0bfb5
zhiwj <314581947@qq.com>
2019-07-29 19:01:40 +0800

    Initial commit

---
+++ ch01/src/main/java/com/bread/ch01/Cmd.java
@@ -0,0 +1,16 @@
+package com.bread.ch01;
+
+import java.util.List;
+
+/**
+ * @author zhiwj
+ * @date 2019/7/29
+ */
+public class Cmd {
+    private boolean helpFlag;
+    private boolean versionFlag;
+    private String cpOption;
+    private String className;
+    private List<String> args;
+
+}
